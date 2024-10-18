435a8d7e01c549e280944eb103ea0fadcd1c748f
nApTu3aH82 <rinsy89@gmail.com>
2024-10-18 15:18:52 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# 1st program
+
+print((9 ** 0.5) * 5)
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# 4th program
+
+main_string = '123.456'
+print(int((float(main_string) * 10) % 10))
